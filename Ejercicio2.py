d76c19008eae3d537eb53da7275e7627b25d7364
grupo4 <camilogarcia@gmail.com>
2020-08-14 13:06:03 -0500

    Quiz 1 de Python

---
+++ Ejercicio2.py
@@ -0,0 +1,17 @@
+'''lista = ['juanita', 'viviana', 'juanes', 'fabiola',"Hola", 'juanes', 'juanes']
+conjunto = set(lista)
+print(conjunto)'''
+
+'''lista = ['juanita', 'viviana', 'juanes', 'fabiola',"Hola", 'juanes', 'juanes']
+conjunto = set(lista)
+lista = list(conjunto)
+print(conjunto)'''
+
+lista = ['juanita', 'viviana', 'juanes', 'fabiola']
+conjunto = set(lista)
+print(conjunto)
+
+
+
+
+
